c872cca425c2accd8886b4e0bc17ae3174c62bf8
Jonathan Arenas <jonathanarenas318@gmail.com>
2022-05-22 23:40:37 -0500

    feat: se agrego el diseño de modal y la funcionalidad para consultar la letra y datos de la canción, asimismo su renderización

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,29 @@ const options = {
 };
 
 const conteinerCard = document.querySelector('#containerCard');
-const songs = [];
-const closeModal= document.getElementById('close-modal');
+const searchForm = document.querySelector('#searchForm');
+const dataSong = [];
 
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const { songSearch } = event.target;
+    const songSearchValue = songSearch.value;
+    cleanView();
+    main(songSearchValue);
 
+});
 
-fetch('https://genius-song-lyrics1.p.rapidapi.com/albums/670828/appearances?per_page=20&page=1', options)
-    .then(response => response.json())
-    .then(data => firstNormalizeData(data))
-    .then(songs => songs.forEach(renderCard))
-    .catch(err => console.error(err));
+const main = (song = 'tiesto') => {
+    fetch('https://genius-song-lyrics1.p.rapidapi.com/search?q=' + song + '&per_page=12&page=1', options)
+        .then(response => response.json(), loader())
+        .then(data => firstNormalizeData(data), cleanView())
+        .then(songs => songs.forEach(renderCardElements))
+        .catch(err => console.error(err));
+}
+
 
 // renderizar el contenido en un Card
-const renderCard = (element) => {
+const renderCardElements = (element) => {
     const divCard = document.createElement('div');
     const divCardContent = document.createElement('div');
     const divCardContentImagen = document.createElement('div');
@@ -28,10 +38,14 @@ const renderCard = (element) => {
     const titleText = document.createElement('h3');
     const divCardContentArtist = document.createElement('div');
     const divContentImagenArtists = document.createElement('div');
+    const divArtistImagen = document.createElement('div')
+    const imgArtist = document.createElement('img');
     const divContentNamesArtists = document.createElement('div');
     const labelNameArtist = document.createElement('label');
     const divMiddle = document.createElement('div');
     const divMiddleText = document.createElement('div');
+
+
 
     divCard.classList.add('card');
     divCardContent.classList.add('card-content');
@@ -42,18 +56,23 @@ const renderCard = (element) => {
     divCardContentArtist.classList.add('artists');
     divContentImagenArtists.classList.add('contentImagenArtists');
     divContentNamesArtists.classList.add('contentNameArtists');
+    divArtistImagen.classList.add('art-imagen');
 
     cardContentImagen.setAttribute('src', element.imagen);
     divMiddleText.innerHTML = 'Lyrics';
-    divMiddleText.setAttribute('lyrics', element.lyricsId);
+    divMiddleText.setAttribute('song', element.song);
     divMiddleText.addEventListener('click', handleLyricsClick);
-    if(element.title.length > 15){
-        element.title = element.title.substring(0,13) + '...';
+    if (element.title.length > 15) {
+        element.title = element.title.substring(0, 13) + '...';
     }
-    
-    titleText.innerHTML = element.title;
-    let artistas = element.artists.length;
 
+    titleText.innerHTML = element.title;
+
+    imgArtist.setAttribute('src', element.artists.image_url);
+    imgArtist.setAttribute('alt', element.artists.name);
+    divArtistImagen.appendChild(imgArtist);
+    divContentImagenArtists.appendChild(divArtistImagen);
+    /*
     for (let index = 0; index < artistas; index++) { 
         const divArtistImagen = document.createElement('div')
         const imgArtist = document.createElement('img');
@@ -66,14 +85,12 @@ const renderCard = (element) => {
             break;
         }
 
-    }
-    if(element.artistsNames.length > 25){
-        element.artistsNames = element.artistsNames.substring(0,22) + '...';
+    }*/
+    if (element.artistsNames.length > 25) {
+        element.artistsNames = element.artistsNames.substring(0, 22) + '...';
     }
     labelNameArtist.innerHTML = element.artistsNames
     divContentNamesArtists.appendChild(labelNameArtist);
-
-    
 
     divCardContentArtist.appendChild(divContentImagenArtists);
     divCardContentArtist.appendChild(divContentNamesArtists);
@@ -91,41 +108,72 @@ const renderCard = (element) => {
 }
 //Limpiar el Contenido del contenedor
 const cleanView = () => {
-    conteinerCard.innerHTML = '';
-};
-
-// Normaliza los datos default 
-const firstNormalizeData = (data) => {
-    albumData = data.response.album_appearances;
-        albumData.forEach(element =>{
-            dataSong = element.song;
-            const {song_art_image_thumbnail_url, artist_names, title, id ,writer_artists} = dataSong;
-            const song = {
-                imagen: song_art_image_thumbnail_url,
-                title: title,
-                artistsNames: artist_names,
-                lyricsId: id,
-                artists: writer_artists
-
-            };
-            songs.push(song);
-        });
-    return songs;
+    conteinerCard.innerHTML =  '';
 };
 
 const handleLyricsClick = (event) => {
-    const idLyrics = event.target.getAttribute('lyrics');
+    openModal();
+    const idLyrics = event.target.getAttribute('song');
     const id = parseInt(idLyrics);
+    searchDataSong(id);
+};
 
-    searchLyrics(id);
-
+const loader = ()=>{
+    conteinerCard.innerHTML = '<div class=Wloader"><div class="bar bar1"></div><div class="bar bar2"></div><div class="bar bar3"></div><div class="bar bar4"></div><div class="bar bar5"></div><div class="bar bar6"></div><div class="bar bar7"></div><div class="bar bar8"></div></div>'
 };
 
 
-const searchLyrics = (id)=>{
-
-    fetch('https://genius-song-lyrics1.p.rapidapi.com/songs/'+id+'/lyrics', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
+const searchDataSong = (id) => {
+    fetch('https://genius-song-lyrics1.p.rapidapi.com/songs/' + id + '?text_format=dom', options)
+    .then(response => response.json())
+    .then(data => searchLyricSong(data, id))
+    .catch(err => console.log(err));
+   
 }
+
+const searchLyricSong = (dataSong, id)=>{
+        fetch('https://genius-song-lyrics1.p.rapidapi.com/songs/' + id + '/lyrics', options)
+        .then(response => response.json())
+        .then(dataLyrics => normalizeDataSong(dataSong, dataLyrics))
+        .then(dataModal => renderContentModal(dataModal))
+        .catch(err => console.error(err));
+}
+
+// Normaliza los datos default result
+const firstNormalizeData = (data) => {
+    const hits = [];
+    albumData = data.response.hits;
+    albumData.forEach(element => {
+        const { result } = element;
+        const hit = {
+            imagen: result.song_art_image_thumbnail_url,
+            title: result.title,
+            artistsNames: result.artist_names,
+            song: result.id,
+            artists: result.primary_artist
+        };
+        hits.push(hit);
+    });
+    return hits;
+
+};
+
+const normalizeDataSong = (dataSong, dataLyric) => {
+    const song = {
+        title: dataSong.response.song.title,
+        imagen: dataSong.response.song.song_art_image_thumbnail_url,
+        date: dataSong.response.song.release_date,
+        youtube: dataSong.response.song.youtube_url,
+        spotify: dataSong.response.song.spotify_uuid
+
+    }
+    const lyric = {
+        lyric: dataLyric?.response?.lyrics?.lyrics?.body?.html || '<p>We are working on it!!</p>',
+        lenguage: dataLyric?.response?.trackingData?.["Lyrics Language"] || '...'
+    }
+    return { ...song, ...lyric };
+}
+
+
+
+main();
---
+++ assets/js/modal.js
@@ -0,0 +1,104 @@
+const body = document.body;
+const modalLyrics = document.getElementById('modal-lyrics')
+const bodyModalLyrics = document.getElementById('lyris-body');
+const divModalHeader = document.getElementById('modal-header');
+
+
+const closeModal = ()=>{
+    modalLyrics.style.top = '-100vh';
+    body.style.overflow = 'auto';
+}
+
+const openModal = ()=>{
+    bodyModalLyrics.style.scrollBehavior = 'initial';
+    cleanModal();
+    bodyModalLyrics.innerHTML = '<div class="lds-dual-ring"></div>';
+    divModalHeader.innerHTML = '<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>';
+    modalLyrics.style.top = 0;
+    body.style.overflow = 'hidden';
+}
+
+
+
+const renderContentModal = (element)=>{
+    const divImagenSong = document.createElement('div');
+    const divImagenSongBox = document.createElement('div')
+    const imagenSong = document.createElement('img')
+    const divInfo = document.createElement('div');
+    const divInfoTitle = document.createElement('div');
+    const divInfoData = document.createElement('div');
+    const divMedia = document.createElement('div');
+    const divSpotify = document.createElement('div')
+    const divSpotifyBox = document.createElement('div');
+    const imagenSpotify = document.createElement('img');
+    const divYoutube = document.createElement('div')
+    const divYoutubeBox = document.createElement('div');
+    const imagenYoutube = document.createElement('img');
+    const divCloseModal = document.createElement('div')
+    const spanCloseModal = document.createElement('span');
+
+    divImagenSong.classList.add('imagen-song');
+    divImagenSongBox.classList.add('box-imagen');
+    imagenSong.setAttribute('src',element.imagen);
+    imagenSong.setAttribute('alt', element.title);
+    divInfo.classList.add('info');
+    divInfoTitle.classList.add('title');
+    divInfoTitle.innerHTML = element.title;
+    divInfoData.classList.add('data');
+    divInfoData.innerHTML = element.date +'.'+element.lenguage;
+    divMedia.classList.add('media');
+    divSpotify.classList.add('spotify');
+    divSpotifyBox.classList.add('box-spotify');
+    imagenSpotify.setAttribute('src', '../assets/img/spotify.png')
+    imagenSpotify.setAttribute('alt', 'spotify')
+    imagenSpotify.setAttribute('enlace', 'https://open.spotify.com/track/'+element.spotify );
+    imagenSpotify.addEventListener('click', openSpotify);
+    divYoutube.classList.add('youtube');
+    divYoutubeBox.classList.add('box-youtube');
+    imagenYoutube.setAttribute('src', '../assets/img/youtube1.png')
+    imagenYoutube.setAttribute('alt', 'youtube');
+    imagenYoutube.setAttribute('enlace', element.youtube);
+    imagenYoutube.addEventListener('click', openYoutube);
+    divCloseModal.classList.add('close-modal');
+    spanCloseModal.innerHTML = 'X';
+    spanCloseModal.addEventListener('click', closeModal);
+
+    divImagenSongBox.appendChild(imagenSong);
+    divImagenSong.appendChild(divImagenSongBox);
+    divInfo.appendChild(divInfoTitle);
+    divInfo.appendChild(divInfoData);
+    
+    divSpotifyBox.appendChild(imagenSpotify);
+    divSpotify.appendChild(divSpotifyBox);
+    divYoutubeBox.appendChild(imagenYoutube);
+    divYoutube.appendChild(divYoutubeBox);
+    divMedia.appendChild(divSpotify);
+    divMedia.appendChild(divYoutube);
+
+    divCloseModal.appendChild(spanCloseModal);
+    cleanModal();
+    divModalHeader.appendChild(divImagenSong);
+    divModalHeader.appendChild(divInfo);
+    divModalHeader.appendChild(divMedia);
+    divModalHeader.appendChild(divCloseModal);
+
+    bodyModalLyrics.innerHTML = element.lyric;
+}
+
+const openYoutube = (event)=>{
+    let enlace = event.target.getAttribute('enlace');
+    console.log(enlace);
+    window.open(enlace)
+}
+const openSpotify = (event)=>{
+    let enlace = event.target.getAttribute('enlace');
+    console.log(enlace);
+    window.open(enlace);
+}
+
+
+const cleanModal =()=>{    
+    divModalHeader.innerHTML = '';
+    bodyModalLyrics.innerHTML = '';
+}
+
